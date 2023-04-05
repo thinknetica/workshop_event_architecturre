9d9b52998685d780c8bd25bfce31e1cc6d4be838
Artem Nistratov <nistratov.artem@gmail.com>
2023-04-05 15:14:19 +0300

    add action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :driver
+
+    def connect
+      self.driver = Driver.find(request.headers['x-driver-id'])
+    rescue
+      reject_unauthorized_connection
+    end
   end
 end
